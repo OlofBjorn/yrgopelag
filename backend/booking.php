e33c8e0ff4ea9abb5f79efc37744540f9acaf547
OlofBjorn <olof.v.bjorn@gmail.com>
2025-12-22 14:57:30 +0100

    Site now rejects false transferCodes

--- backend/booking.php
+++ backend/booking.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 require(__DIR__ . '/../vendor/autoload.php');
 
 use GuzzleHttp\Client;
@@ -9,7 +11,8 @@ use GuzzleHttp\Client;
 
 $databaseLocation = "sqlite:".__DIR__.'/database.db';
 $database = new PDO($databaseLocation);
-
+// CODE ONLY CHECKS IF SET, NOT IF EMPTY STRING
+//TODO: MAKE SURE ARRIVAL AND DEPARTURE HAVE AN ACTUAL VALUE AND THAT ARRIVAL MUST BE SMALLER THAN DEPARTURE
 if (isset($_POST['nameInput'],$_POST['roomInput'],$_POST['arrivalInput'],$_POST['departureInput'], $_POST['checkbox'])) {
     $nameInput = trim($_POST['nameInput']);
     $codeInput = trim($_POST['codeInput']);
@@ -23,75 +26,88 @@ if (isset($_POST['nameInput'],$_POST['roomInput'],$_POST['arrivalInput'],$_POST[
         echo 'No code :(';
     } else {
 
-        // -----------------------------------------------------
+        //PERHAPS CHECK ARRIVAL AND DEPATURE HERE?
 
-        $query = 'INSERT INTO guests (name) VALUES (:nameInput)';
+        if($arrivalInput>$departureInput){
+            echo 'No time traveling!';
+        }else{
 
-        $statement = $database->prepare($query);
+            // ---------------------------------------------------
 
-        $statement->bindParam(':nameInput', $nameInput, PDO::PARAM_STR);
+            //TODO: Calculate cost: Room Price X Days + Activities
+            
+            //TODO: Check transferCode
+            $client = new Client([
+                'base_uri' => 'https://www.yrgopelag.se/centralbank/transferCode',
+            ]);
 
-        $statement->execute();
+            try{
+                $r = $client->request('POST', 'https://www.yrgopelag.se/centralbank/transferCode', [
+                    'form_params' => [
+                        'transferCode' => $codeInput, 
+                        'totalCost' => '5'
+                        ]
+                ]);
+            }
+            catch (\Exception $codeError){
+                echo "Error, invalid or used code";
+            }
 
-        $last_user_id = $database->lastInsertId();
-        echo "New record created successfully. Last inserted ID is: " . $last_user_id;
+            var_dump($r->getBody()->getContents()); 
 
-        var_dump($statement);
 
-        // ---------------------------------------------------
 
-        //TODO: Calculate cost: Room Price X Days + Activities
-        
-        //TODO: Check transferCode
-        $client = new Client([
-            'base_uri' => 'https://www.yrgopelag.se/centralbank/transferCode',
-        ]);
+            // -----------------------------------------------------
 
-        $r = $client->request('POST', 'https://www.yrgopelag.se/centralbank/transferCode', [
-            'form_params' => [
-                'transferCode' => $codeInput, 
-                'totalCost' => '5'
-                ]
-        ]);
-
-        var_dump($r->getBody()->getContents()); 
-    
-        //------------------------------------------------------------
-
-        $query = 'INSERT INTO visits (guest_id, room_id, arrival, departure) VALUES (:guest_id, :room_id, :arrivalInput, :departureInput)';
-
-        $statement = $database->prepare($query);
-
-        $statement->bindParam(':guest_id', $last_user_id, PDO::PARAM_INT);
-
-        $statement->bindParam(':room_id', $roomInput, PDO::PARAM_INT);
-
-        $statement->bindParam(':arrivalInput', $arrivalInput, PDO::PARAM_STR);
-
-        $statement->bindParam(':departureInput', $departureInput, PDO::PARAM_STR);
-
-        $statement->execute();
-        
-        $last_visit_id = $database->lastInsertId();
-        echo "New record created successfully. Last inserted ID is: " . $last_visit_id;
-
-        //----------------------------------------------------------------
-
-        var_dump($checkboxes);
-
-        foreach($checkboxes as $checkbox){
-
-            $query = 'INSERT INTO visit_activities (visit_id, activity_id) VALUES (:visit_id, :activity_id)'; 
+            $query = 'INSERT INTO guests (name) VALUES (:nameInput)';
 
             $statement = $database->prepare($query);
 
-            $statement->bindParam(':visit_id', $last_visit_id, PDO::PARAM_INT);
-
-            $statement->bindParam(':activity_id', $checkbox, PDO::PARAM_INT);
+            $statement->bindParam(':nameInput', $nameInput, PDO::PARAM_STR);
 
             $statement->execute();
-        };
 
+            $last_user_id = $database->lastInsertId();
+            echo "New record created successfully. Last inserted ID is: " . $last_user_id;
 
+            var_dump($statement);
+        
+            //------------------------------------------------------------
+
+            $query = 'INSERT INTO visits (guest_id, room_id, arrival, departure) VALUES (:guest_id, :room_id, :arrivalInput, :departureInput)';
+
+            $statement = $database->prepare($query);
+
+            $statement->bindParam(':guest_id', $last_user_id, PDO::PARAM_INT);
+
+            $statement->bindParam(':room_id', $roomInput, PDO::PARAM_INT);
+
+            $statement->bindParam(':arrivalInput', $arrivalInput, PDO::PARAM_STR);
+
+            $statement->bindParam(':departureInput', $departureInput, PDO::PARAM_STR);
+
+            $statement->execute();
+            
+            $last_visit_id = $database->lastInsertId();
+            echo "New record created successfully. Last inserted ID is: " . $last_visit_id;
+
+            //----------------------------------------------------------------
+
+            var_dump($checkboxes);
+
+            foreach($checkboxes as $checkbox){
+
+                $query = 'INSERT INTO visit_activities (visit_id, activity_id) VALUES (:visit_id, :activity_id)'; 
+
+                $statement = $database->prepare($query);
+
+                $statement->bindParam(':visit_id', $last_visit_id, PDO::PARAM_INT);
+
+                $statement->bindParam(':activity_id', $checkbox, PDO::PARAM_INT);
+
+                $statement->execute();
+            };
+
+        }
     }
 }
